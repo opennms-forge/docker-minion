714509cb9318b6b92fc15eb8cbd0fb7acc36f0a6
Ronny Trommer <ronny@opennms.org>
2016-09-06 15:57:33 +0200

    Created docker-entrypoint and environment variables to configure Minion broker and http urls

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,12 @@ FROM indigo/centos-jdk8:latest
 MAINTAINER Ronny Trommer <ronny@opennms.org>
 
 ENV OPENNMS_VERSION develop
-ENV OPENNMS_HOME=/opt/opennms
+ENV OPENNMS_MINION_HOME /opt/minion
+ENV OPENNMS_LOCATION MINION
+ENV OPENNMS_MINION_CONFIG ${OPENNMS_MINION_HOME}/etc/org.opennms.minion.controller.cfg
+ENV OPENNMS_MINION_CONFIG_DIR ${OPENNMS_MINION_HOME}/etc
+ENV OPENNMS_BROKER_URL tcp://127.0.0.1:61616
+ENV OPENNMS_HTTP_URL tcp://127.0.0.1:8980/opennms
 
 RUN rpm -Uvh http://yum.opennms.org/repofiles/opennms-repo-${OPENNMS_VERSION}-rhel7.noarch.rpm && \
     rpm --import http://yum.opennms.org/OPENNMS-GPG-KEY && \
@@ -10,7 +15,9 @@ RUN rpm -Uvh http://yum.opennms.org/repofiles/opennms-repo-${OPENNMS_VERSION}-rh
 
 WORKDIR /opt/minion/bin
 
-CMD ["start"]
+ENTRYPOINT ["./karaf"]
+
+CMD ["status"]
 
 ##------------------------------------------------------------------------------
 ## EXPOSED PORTS
@@ -20,3 +27,10 @@ CMD ["start"]
 ## -- SNMP Trapd           162/UDP
 ## -- Syslog               514/UDP
 EXPOSE 8201 18980 162/udp 514/udp
+
+
+
+location = Fulda
+id = b17a11b4-741b-11e6-9c50-0242ac110003
+broker-url = tcp://172.24.23.103:61616
+http-url = http://172.24.23.103:8980/opennms
---
+++ docker-entrypoint.sh
@@ -0,0 +1,78 @@
+#!/bin/bash -e
+# =====================================================================
+# Build script running OpenNMS Minion in Docker environment
+#
+# Source: https://github.com/indigo423/docker-minion
+# Web: https://www.opennms.org
+#
+# =====================================================================
+
+START_DELAY=5
+
+# Error codes
+E_ILLEGAL_ARGS=126
+
+# Help function used in error messages and -h option
+usage() {
+    echo ""
+    echo "Docker entry script for OpenNMS Minion service container"
+    echo ""
+    echo "-f: Initialize and start OpenNMS Minion in foreground."
+    echo "-h: Show this help."
+    echo ""
+}
+
+initConfig() {
+    if [ ! -d ${OPENNMS_MINION_HOME} ]; then
+        echo "OpenNMS Minion home directory doesn't exist in ${OPENNMS_MINION_HOME}."
+        exit ${E_ILLEGAL_ARGS}
+    fi
+
+    if [ ! -f ${OPENNMS_MINION_CONFIG_DIR/configured} ]; then
+        sed -i "s,sshHost = 127.0.0.1,sshHost = 0.0.0.0," ${OPENNMS_MINION_CONFIG_DIR}/org.apache.karaf.shell.cfg
+        sed -i "s,location = MINION,location = ${OPENNMS_LOCATION}," ${OPENNMS_MINION_CONFIG}
+        echo "broker-url = ${OPENNMS_BROKER_URL}" >> ${OPENNMS_MINION_CONFIG}
+        echo "http-url = ${OPENNMS_HTTP_URL}" >> ${OPENNMS_MINION_CONFIG}
+        echo "Configured $(date)" > ${OPENNMS_MINION_CONFIG_DIR}/configured
+    fi
+}
+
+start() {
+    cd ${OPENNMS_MINION_HOME}/bin
+    exec ./start
+}
+
+# Evaluate arguments for build script.
+if [[ "${#}" == 0 ]]; then
+    usage
+    exit ${E_ILLEGAL_ARGS}
+fi
+
+# Evaluate arguments for build script.
+while getopts fhis flag; do
+    case ${flag} in
+        f)
+            initConfig
+            start
+            exit
+            ;;
+        h)
+            usage
+            exit
+            ;;
+        *)
+            usage
+            exit ${E_ILLEGAL_ARGS}
+            ;;
+    esac
+done
+
+# Strip of all remaining arguments
+shift $((OPTIND - 1));
+
+# Check if there are remaining arguments
+if [[ "${#}" > 0 ]]; then
+    echo "Error: To many arguments: ${*}."
+    usage
+    exit ${E_ILLEGAL_ARGS}
+fi
