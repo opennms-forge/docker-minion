ed43c5853bc1dddcc60fc5f0d135be2a39dce5eb
Ronny Trommer <ronny@opennms.org>
2018-02-12 10:28:09 +0100

    Issue#1: Set credentials for OpenNMS commmunication
    Enhanced the entrypoint file to provide two options to set credentials. The first option allows to use credentials for
    the Broker and HTTP communication via environment variables using the -c option.
    For the reason those credentials can be exposed in log files and docker inspect a second option to use a keystore file.
    Added a -s command line option to give users the possibility to initialize a keystore file in interactive mode, which can
    be mounted into the container. This way no credentials are exposed while running the container.
    
    Added also minor issues to ensure configuration file is always created.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,9 +15,41 @@ usage() {
     echo ""
     echo "Docker entry script for OpenNMS Minion service container"
     echo ""
+    echo "-c: Start Minion and use environment credentials to register Minion on OpenNMS."
+    echo "    WARNING: Credentials can be exposed via docker inspect and log files. Please consider to use -s option."
+    echo "-s: Initialize a keystore file with credentials in /keystore/scv.jce."
+    echo "    Mount /keystore to your local system or a volume to save the keystore file."
+    echo "    You can mount the keystore file to ${MINION_HOME}/etc/scv.jce and just use -f to start the Minion."
     echo "-f: Initialize and start OpenNMS Minion in foreground."
     echo "-h: Show this help."
     echo ""
+}
+
+useEnvCredentials(){
+  echo "WARNING: Credentials can be exposed via docker inspect and log files. Please consider to use a keystore file."
+  echo "         You can initialize a keystore file with the -s option."
+  ${MINION_HOME}/bin/scvcli set opennms.http ${OPENNMS_HTTP_USER} ${OPENNMS_HTTP_PASS}
+  ${MINION_HOME}/bin/scvcli set opennms.broker ${OPENNMS_BROKER_USER} ${OPENNMS_BROKER_PASS}
+}
+
+setCredentials() {
+  # Directory to initialize a new keystore file which can be mounted to the local host
+  if [ -z /keystore ]; then
+    mkdir /keystore
+  fi
+
+  read -p "Enter OpenNMS HTTP username: " OPENNMS_HTTP_USER
+  read -s -p "Enter OpenNMS HTTP password: " OPENNMS_HTTP_PASS
+  echo ""
+
+  read -p "Enter OpenNMS Broker username: " OPENNMS_BROKER_USER
+  read -s -p "Enter OpenNMS Broker password: " OPENNMS_BROKER_PASS
+  echo ""
+
+  ${MINION_HOME}/bin/scvcli set opennms.http ${OPENNMS_HTTP_USER} ${OPENNMS_HTTP_PASS}
+  ${MINION_HOME}/bin/scvcli set opennms.broker ${OPENNMS_BROKER_USER} ${OPENNMS_BROKER_PASS}
+
+  cp ${MINION_HOME}/etc/scv.jce /keystore
 }
 
 initConfig() {
@@ -59,8 +91,16 @@ if [[ "${#}" == 0 ]]; then
 fi
 
 # Evaluate arguments for build script.
-while getopts fhis flag; do
+while getopts csfh flag; do
     case ${flag} in
+        c)
+            useEnvCredentials
+            initConfig
+            start
+            ;;
+        s)
+            setCredentials
+            ;;
         f)
             initConfig
             start
